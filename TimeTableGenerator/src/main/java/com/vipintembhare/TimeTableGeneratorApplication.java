2f0ae3ec1b0ad2de68ad956acd1dc9c01ed2d0a5
vipinrtembhare <you@example.com>
2019-01-08 00:26:13 +0530

    Initial commit

---
+++ TimeTableGenerator/src/main/java/com/vipintembhare/TimeTableGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.vipintembhare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTableGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTableGeneratorApplication.class, args);
+	}
+
+}
+
